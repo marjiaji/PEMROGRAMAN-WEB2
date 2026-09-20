3d75d8a0767fb129179a521a2db10b22faa4ff92
marji <143249402+marjiaji@users.noreply.github.com>
2026-09-20 11:29:30 +0700

    Pertemuan ke 5

---
+++ Pertemuan5/counter.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title>Contoh Counter</title>
+</head>
+
+<body>
+
+<?php
+
+$nama_file = "counter.dat";
+
+if (file_exists($nama_file)) {
+
+    $berkas = fopen($nama_file, "r");
+
+    $pencacah = (integer) trim(fgets($berkas, 255));
+
+    $pencacah++;
+
+    fclose($berkas);
+
+} else {
+
+    $pencacah = 1;
+}
+
+// Simpan pencacah
+$berkas = fopen($nama_file, "w");
+
+fputs($berkas, $pencacah);
+
+fclose($berkas);
+
+// Tulis ke halaman web
+print("Anda pengunjung ke-$pencacah <br>");
+
+?>
+
+</body>
+</html>
---
+++ Pertemuan5/fgets2.php
@@ -0,0 +1,11 @@
+<?php
+
+$file = fopen("test1.txt", "r");
+
+while (!feof($file)) {
+    echo fgets($file) . "<br />";
+}
+
+fclose($file);
+
+?>
